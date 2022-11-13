f8343bd953ad541b94a39a0aed4e9089bb450963
ekolcole <91461613+ekolcole@users.noreply.github.com>
2022-11-13 13:55:50 +0800

    Add files via upload

---
+++ BinarySearchExample/src/binarysearchexample/BinarySearchExample.java
@@ -0,0 +1,30 @@
+
+package binarysearchexample;
+
+public class BinarySearchExample {
+    public static void binarySearch(int arr[],int first, int last, int key) {
+
+		int mid= (first + last)/2;
+		while ( first <= last ) {
+			if (arr [mid] < key ) {
+				first = mid +1;
+			} else if ( arr[mid]==key) {
+			System.out.println("Element is found at index:" + mid);
+			break;
+			}else{
+				last = mid - 1;
+			}
+			mid = (first + last )/ 2;
+	}
+	if (first  > last) {
+		System.out.println("Element is not found!");
+	}
+}
+    public static void main(String[] args) {
+		int A[] = {9,6,8,7,10};
+		int key = 6;
+                int last= A.length-1;
+		binarySearch (A,0,last,key);
+    }
+    
+}
